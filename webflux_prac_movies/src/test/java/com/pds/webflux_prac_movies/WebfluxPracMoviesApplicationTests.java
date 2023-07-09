9c3dda0264eee99fe41093b824a667d5616dd936
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-07-09 20:25:28 +0900

    [webflux-prac] init appication

---
+++ webflux_prac_movies/src/test/java/com/pds/webflux_prac_movies/WebfluxPracMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pds.webflux_prac_movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPracMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
