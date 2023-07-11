b30b0ba39a2400ceb87788e8bbf9c189e4319ec3
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-07-11 11:49:33 +0900

    [webflux-prac] add webclient exception handling

--- webflux_prac_movies/clientservice/src/main/java/com/pds/clientservice/client/MoviesInfoRestClient.java
+++ webflux_prac_movies/clientservice/src/main/java/com/pds/clientservice/client/MoviesInfoRestClient.java
@@ -1,7 +1,9 @@
 package com.pds.clientservice.client;
 
 import com.pds.clientservice.domain.MovieInfo;
+import com.pds.clientservice.exception.MoviesInfoClientException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -22,6 +24,17 @@ public class MoviesInfoRestClient {
         return webClient.get()
                 .uri(url, movieId)
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                    if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                        return Mono.error(
+                                new MoviesInfoClientException(
+                                        "There is no Movie Info by passed id : " + movieId,
+                                        clientResponse.statusCode().value()));
+                    }
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(s -> Mono.error(
+                                    new MoviesInfoClientException(s, clientResponse.statusCode().value())));
+                })
                 .bodyToMono(MovieInfo.class)
                 .log();
     }
--- webflux_prac_movies/clientservice/src/main/java/com/pds/clientservice/client/ReviewRestClient.java
+++ webflux_prac_movies/clientservice/src/main/java/com/pds/clientservice/client/ReviewRestClient.java
@@ -1,11 +1,14 @@
 package com.pds.clientservice.client;
 
 import com.pds.clientservice.domain.Review;
+import com.pds.clientservice.exception.ReviewsClientException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component
 public class ReviewRestClient {
@@ -25,6 +28,13 @@ public class ReviewRestClient {
                 .get()
                 .uri(url)
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, (clientResponse -> {
+                    if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                        return Mono.empty();
+                    }
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(response -> Mono.error(new ReviewsClientException(response)));
+                }))
                 .bodyToFlux(Review.class)
                 .log();
     }
