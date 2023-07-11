ace41000c3f2a485d04a663b6a0e5fb64237bd80
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-07-11 15:20:35 +0900

    [sse] add sample pub/sub controller

--- server-sent-event/src/main/java/com/pds/serversentevent/PracSseController.java
+++ server-sent-event/src/main/java/com/pds/serversentevent/PracSseController.java
@@ -2,17 +2,33 @@ package com.pds.serversentevent;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 
 import java.time.Duration;
 
 @RestController
 public class PracSseController {
 
+    Sinks.Many<Integer> manyReplayIntegerSink = Sinks.many().replay().latest();
+
     @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Long> stream() {
         return Flux.interval(Duration.ofSeconds(1)).log();
+    }
+
+    @PostMapping("/sink/many/replay")
+    public Mono<Integer> publishSinkManyReplay(@RequestBody Integer integer) {
+        return Mono.just(integer).doOnNext(itg -> manyReplayIntegerSink.tryEmitNext(itg));
+    }
+
+    @GetMapping(value = "/sink/many/replay", produces = MediaType.APPLICATION_NDJSON_VALUE)
+    public Flux<Integer> subscribeSinkManyReplay() {
+        return manyReplayIntegerSink.asFlux();
     }
 
 }
--- server-sent-event/src/test/java/com/pds/serversentevent/PracSseControllerTest.java
+++ server-sent-event/src/test/java/com/pds/serversentevent/PracSseControllerTest.java
@@ -1,11 +1,15 @@
 package com.pds.serversentevent;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @WebFluxTest(controllers = PracSseController.class)
 @AutoConfigureWebTestClient
@@ -26,6 +30,39 @@ class PracSseControllerTest {
                 .getResponseBody();
         StepVerifier.create(flux)
                 .expectNext(0L, 1L, 2L, 3L)
+                .thenCancel()
+                .verify();
+    }
+
+    @Test
+    void publishSinkManyReplay() {
+        webTestClient
+                .post()
+                .uri("/sink/many/replay")
+                .bodyValue(1)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(Integer.class)
+                .consumeWith(integerEntityExchangeResult ->
+                        assertEquals(1, integerEntityExchangeResult.getResponseBody()));
+    }
+
+    @Test
+    void subscribeSinkManyReplay() {
+        publishSinkManyReplay();
+        var flux = webTestClient
+                .get()
+                .uri("/sink/many/replay")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Integer.class)
+                .getResponseBody();
+        StepVerifier.create(flux)
+                .assertNext(integer -> {
+                    assertEquals(1, integer);
+                })
                 .thenCancel()
                 .verify();
     }
