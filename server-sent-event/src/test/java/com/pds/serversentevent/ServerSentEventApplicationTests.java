38517c3f8ce8680d7c171123666e7cc97f1eb2c6
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-07-11 14:57:09 +0900

    [sse] init sse app

---
+++ server-sent-event/src/test/java/com/pds/serversentevent/ServerSentEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pds.serversentevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSentEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
